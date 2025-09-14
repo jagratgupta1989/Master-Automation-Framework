b62767d0eb9d6d204b0c112a0518bb04af47e60b
jagratgupta1989 <jagrat.webtech@gmail.com>
2025-09-14 16:32:19 +0530

    GriJenkinsfile added and code modified

--- src/main/java/com/jarhub/factories/DriverFactory.java
+++ src/main/java/com/jarhub/factories/DriverFactory.java
@@ -6,7 +6,10 @@ import com.jarhub.utils.PropertyUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.Objects;
 
 public class DriverFactory {
@@ -20,11 +23,23 @@ public class DriverFactory {
         WebDriver driver=null;
         if(Objects.isNull(DriverManager.getDriver()))
         {
-            if(PropertyUtils.get(ConfigProperties.RUNMODE).equalsIgnoreCase("local")) {
-                if (browser.equalsIgnoreCase("chrome")) {
+            if (browser.equalsIgnoreCase("chrome"))
+            {
+                if (PropertyUtils.get(ConfigProperties.RUNMODE).equalsIgnoreCase("remote")) {
+                    DesiredCapabilities cap = new DesiredCapabilities();
+                    cap.setBrowserName("chrome");
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+                } else {
                     driver = new ChromeDriver();
                 }
-                if (browser.equalsIgnoreCase("firefox")) {
+            }
+            if (browser.equalsIgnoreCase("firefox"))
+            {
+                if (PropertyUtils.get(ConfigProperties.RUNMODE).equalsIgnoreCase("remote")) {
+                    DesiredCapabilities cap = new DesiredCapabilities();
+                    cap.setBrowserName("firefox");
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+                } else {
                     driver = new FirefoxDriver();
                 }
             }
